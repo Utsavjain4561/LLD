3d43628690fc3307d813724c48e1ac06ad42bc0f
Utsavjain4561 <utsavslife@gmail.com>
2023-01-16 22:08:00 +0530

    Add: maven dependencies

---
+++ SnakeAndLadder/src/main/java/org/code/Main.java
@@ -0,0 +1,10 @@
+package org.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        log.info("This is first commit");
+    }
+}
